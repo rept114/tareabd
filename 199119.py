78177189afa9a906051b93241544e872030c5823
rept114 <busquerasegura114@gmail.com>
2023-03-14 14:35:17 -0700

    sendtask

---
+++ 199119.py
@@ -0,0 +1,19 @@
+
+carro = input("Nombre de tu carro: ")
+modelo = input("Modelo de tu carro: ")
+
+class Mascota(object):
+    def __init__(self, carro, modelo):
+        self.carro = carro
+        self.modelo = modelo
+
+    def darNombre(self):
+        return self.carro
+    
+    def darEspecie(self):
+        return self.modelo
+    
+    def __str__(self):
+        return "%s es un %s" % (self.carro, self.modelo)
+
+print (Mascota(carro, modelo))
